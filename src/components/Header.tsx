9967e50fdbe37a08b7d19539842913f818ea3441
Radnoumane Mossabely <radnou.m@gmail.com>
2023-05-23 22:16:46 +0200

    feat: Add dark mode toggle and theming support

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,37 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useTheme } from "next-themes";
+
 export const Header = () => {
   const { data: sessionData } = useSession();
+  const { theme, setTheme } = useTheme();
 
   return (
     <div className="navbar bg-primary text-primary-content">
       <div className="flex-1 pl-5 text-3xl font-bold">
-        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+        <div>
+          {theme === "business" ? (
+            <div
+              className="flex-1 lg:flex-none"
+              onClick={() => setTheme("winter")}
+            >
+              <SunIcon className="text-white-400 h-8 w-8 cursor-pointer sm:h-10 sm:w-10" />
+            </div>
+          ) : (
+            <div
+              className="flex-1 lg:flex-none"
+              onClick={() => setTheme("business")}
+            >
+              <MoonIcon className="text-white-400 h-8 w-8 cursor-pointer sm:h-8 sm:w-8" />
+            </div>
+          )}
+        </div>
+        <span className={"pl-5"}>
+          {sessionData?.user?.name
+            ? `Notes for ${sessionData.user.name} `
+            : " "}{" "}
+        </span>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown-end dropdown">
